919d5e06716e00bacea8922d13e07e9e2ecd6598
Juwami <sean.nguyenf@yahoo.com>
2019-08-01 19:46:55 -0400

    start on apiRoutes.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,24 +1,36 @@
 var db = require("../models");
 
 module.exports = function(app) {
-  // Get all examples
-  app.get("/api/examples", function(req, res) {
+  // Post: Authenticate User
+  app.post("/api/login", function(req, res) {
+    // console.log("Req.body:", req.body)
+    db.Events.fineOne
     db.Example.findAll({}).then(function(dbExamples) {
       res.json(dbExamples);
     });
   });
 
-  // Create a new example
+  // Post: Add New User
   app.post("/api/examples", function(req, res) {
     db.Example.create(req.body).then(function(dbExample) {
       res.json(dbExample);
     });
   });
 
-  // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
+  // Post: New Event
+  app.post("/api/eventMaint", function(req, res) {
+    // console.log("Req.body:", req.body)
+    db.Events.create().then(function() {
+      res.sendStatus(200);
+    });
+  });
+
+  // Update: Event
+  app.post("/api/eventMaint/:id", function(req, res) {
+    // console.log("Req.params.id:", req.params.id)
+    // need req.params from Louis code
+    db.Events.update().then(function() {
+      res.sendStatus(200);
     });
   });
 };
