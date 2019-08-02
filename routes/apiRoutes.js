a3c20d019f7de7d4955335703a503ad531308aaa
Juwami <sean.nguyenf@yahoo.com>
2019-08-01 20:08:20 -0400

    install passport and drum up skeleton code for apiRoutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,25 +1,27 @@
-var db = require("../models");
+let db = require("../models");
+let passport = require("passport");
 
 module.exports = function(app) {
   // Post: Authenticate User
   app.post("/api/login", function(req, res) {
     // console.log("Req.body:", req.body)
-    db.Events.fineOne
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
-    });
+    passport.authenticate("local", {failureRedirect: "/login"}),
+    function(req, res) {
+      res.redirect("/eventMaint")
+    }
   });
 
   // Post: Add New User
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
+  app.post("/api/register", function(req, res) {
+    db.Events.create().then(function() {
+      res.sendStatus(200);
     });
   });
 
   // Post: New Event
   app.post("/api/eventMaint", function(req, res) {
     // console.log("Req.body:", req.body)
+    // neeq req.body
     db.Events.create().then(function() {
       res.sendStatus(200);
     });
@@ -28,7 +30,8 @@ module.exports = function(app) {
   // Update: Event
   app.post("/api/eventMaint/:id", function(req, res) {
     // console.log("Req.params.id:", req.params.id)
-    // need req.params from Louis code
+    // need req.params
+    // need req.body from Louis code
     db.Events.update().then(function() {
       res.sendStatus(200);
     });
