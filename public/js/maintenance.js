59a75d95c86b5ce52df5dafdfb45e48f7d6c411e
Juwami <sean.nguyenf@yahoo.com>
2019-08-07 20:58:46 -0400

    declaring variables for all inputs

--- public/js/maintenance.js
+++ public/js/maintenance.js
@@ -1,14 +1,17 @@
 $(document).ready(function() {
-    var lineup = $("#lineup-Input").val().trim();
-    var dateTime = $("#Date-and-Time-Input").val().trim();
-    var cost = $("#cost-Input").val().trim();
-    var ticketLink = $("#Ticket-Link").val().trim();
-    var posterLink = $("#Poster-Link").val().trim();
-    var phone = $("#phone-Input").val().trim();
-    var state = $("#state-Input").val().trim();
-    var city = $("#city-Input").val().trim();
-    var zipcode = $("#zipcode-Input").val().trim();
-    var address = $("#address-Input").val().trim();
+    var maintenance = (".eventMaintenance");
+    var lineup = $("#lineup-Input");
+    var dateTime = $("#Date-and-Time-Input");
+    var cost = $("#cost-Input");
+    var ticketLink = $("#Ticket-Link");
+    var posterLink = $("#Poster-Link");
+    var phone = $("#phone-Input");
+    var state = $("#state-Input");
+    var city = $("#city-Input");
+    var zipcode = $("#zipcode-Input");
+    var address = $("#address-Input");
 
-    
+    let newEvent = {
+        
+    }
 })
